85b9d2747bcc7f85142909e312af115416567f5a
OrlandoCarcamo <00370825@uca.edu.sv>
2025-10-10 14:15:07 -0600

    labo5

---
+++ Practica05/comoustedesquieran.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int num;
+    int mult = 1;
+    cout <<"ingrese un numero" << endl;
+    cin >> num;
+        for ( mult; mult <= 20; mult ++){
+            cout << num * mult << " ";
+
+        }
+     
+
+ return 0;
+}    
